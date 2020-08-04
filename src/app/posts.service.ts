18157dd69690bea9d471612605ce1b24fb302f36
thamtran <thamtran9x@gmail.com>
2020-08-04 16:43:27 +0200

    using Subject for Error handling

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -2,15 +2,19 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Post } from './post-model';
 import { map } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class PostsService {
+  error = new Subject<string>();
   constructor(private http: HttpClient) {}
 
   createAndStorePost(title: string, content: string) {
     const postData: Post = { title: title, content: content };
     this.http.post('https://ng-complete-guide-9702c.firebaseio.com/posts.json', postData).subscribe((responseData) => {
       console.log(responseData);
+    }, error => {
+      this.error.next(error.message);
     });
   }
 
